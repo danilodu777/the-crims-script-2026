776a5654e2240b740dbcb13dd8c198d02e6619ba
danilodu777 <daneanthony2810@hotmail.com>
2026-02-06 17:58:20 -0300

    Create automation.js with SoloRobberyAutomation and GangRobberyAutomation classes

---
+++ automation.js
@@ -0,0 +1,21 @@
+// automation.js
+
+class SoloRobberyAutomation {
+    constructor() {
+        // Initialize Solo Robbery Automation
+    }
+    start() {
+        // Start the solo robbery automation
+    }
+}
+
+class GangRobberyAutomation {
+    constructor() {
+        // Initialize Gang Robbery Automation
+    }
+    start() {
+        // Start the gang robbery automation
+    }
+}
+
+module.exports = { SoloRobberyAutomation, GangRobberyAutomation };
