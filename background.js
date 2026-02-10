870b2060bc4ba4e2e0a9275706dc1a241a48efde
danilodu777 <daneanthony2810@hotmail.com>
2026-02-09 23:00:10 -0300

    Update service worker installation logging

--- background.js
+++ background.js
@@ -1,26 +1,3 @@
-// background.js
-
-self.addEventListener('install', () => {
-  console.log('Service worker instalando...');
-});
-
-self.addEventListener('activate', () => {
-  console.log('Service worker ativado...');
-});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'TOGGLE_AUTOMATION') {
-    console.log('Comando de automação recebido:', event.data.payload);
-
-    chrome.storage.local.set({ automationEnabled: event.data.payload });
-
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          type: 'AUTOMATION_STATUS',
-          payload: event.data.payload
-        });
-      }
-    });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('DUARTT AutoBot Extension installed!');
 });
